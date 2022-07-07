52b287a73d1e9bc424a95d6b1149fad6664bf61d
ange raul <utm20040056@utma.edu.ma>
2022-07-07 10:48:29 -0500

    estamos trabajando .3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  urlNewsApi: "https://newsapi.org",
+  urlMarvelApi: "https://gateway.marvel.com",
+  publickey:'d042fdb26891bb4a3d783223607e6b87',
+  privatekey: '6568629d6c749a6476f3cf690367aeed0c7038b0'
 };
